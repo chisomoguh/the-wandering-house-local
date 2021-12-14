70f129af0517d28f5c09e1763eca9869fd3e0aa4
Asguerrero07 <you@example.com>
2021-12-14 16:25:57 -0500

    Used lifecycle methods to detect width of screen

--- the-wandering-house/src/App.js
+++ the-wandering-house/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+constructor(props) {
+  super(props);
+  this.state = { width: 0, height: 0 };
+  this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+}
+
+componentDidMount() {
+  this.updateWindowDimensions();
+  window.addEventListener('resize', this.updateWindowDimensions);
+}
+
+componentWillUnmount() {
+  window.removeEventListener('resize', this.updateWindowDimensions);
+}
+
+updateWindowDimensions() {
+  this.setState({ width: window.innerWidth, height: window.innerHeight });
+}
+
+render() {
+
+  if (this.state.width > 500){
+    return(
+
+      <div>
+        Computer
+      </div>
+
+
+    );
+  }
+
+  else{
+
+    return (
+      <div className='App'>
+       Width: {this.state.width} - 
+       Phone
+      </div>
+    );
+
+  }
+
+  
+}
+
 }
 
 export default App;
