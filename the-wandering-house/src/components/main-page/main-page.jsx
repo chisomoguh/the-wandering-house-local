e39f88138a66dc71bc0e888868dc0aea458c0381
Asguerrero07 <you@example.com>
2022-01-16 11:01:41 -0600

    Implemented changes on main page to allow scrolling

--- the-wandering-house/src/components/main-page/main-page.jsx
+++ the-wandering-house/src/components/main-page/main-page.jsx
@@ -4,7 +4,7 @@ import './main-page.css';
 
 export const MainPage = ({onClick1, onClick2, height}) => (
   
-  <div className='home-page-div' style={{height: `${height}%`}}>
+  <div className='home-page-div' style={{height: `${height}px`}}>
     <div className='home-page-container'>
       <div className='main-page-ult-text'>
         <div className='main-page-title'>Welcome to the Wandering House</div>
