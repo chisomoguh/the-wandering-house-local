3c794b9c2fee497a5d3e00614feeaf7bb56926c5
Asguerrero07 <you@example.com>
2022-01-16 11:24:43 -0600

    Implemented changes on main page to allow scrolling

--- the-wandering-house/src/App.js
+++ the-wandering-house/src/App.js
@@ -146,11 +146,14 @@ render() {
 
       <div className='home-div'>
 
+        
+        
+        <div className='main-container'>
+
         { this.state.showMainPage === true ? 
           <MainPage onClick1={ this.hideMainPage } onClick2={ this.showReadMore } height={this.state.height}/>
         : null} 
         
-        <div className='main-container'>
           <div className='blue-line'></div>
           <div className='main-title-div'>
               <img
